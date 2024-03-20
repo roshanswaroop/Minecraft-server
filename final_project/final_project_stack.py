3c667c2879f6423a9837ac409ae3702e7b2dfc20
roshanswaroop <swaroop1@stanford.edu>
2024-03-20 06:08:35 +0000

    final push :(

--- final_project/final_project_stack.py
+++ final_project/final_project_stack.py
@@ -10,6 +10,7 @@ from aws_cdk import (
     aws_certificatemanager as acm,
 )
 
+
 class FinalProjectStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -79,18 +80,23 @@ class FinalProjectStack(cdk.Stack):
         # Configure security group to allow traffic on port 25565
         service.service.connections.allow_from_any_ipv4(ec2.Port.tcp(25565))
 
-        # Create an RDS instance for Minecraft server data
-        db = rds.DatabaseInstance(
+        db_cluster = rds.DatabaseCluster(
             self,
-            "MyDatabase",
-            engine=rds.DatabaseInstanceEngine.mysql(version=rds.MysqlEngineVersion.VER_8_0_16),
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
-            vpc=vpc,
+            "MyDatabaseCluster",
+            engine=rds.DatabaseClusterEngine.aurora_mysql(
+                version=rds.AuroraMysqlEngineVersion.VER_2_11_2  # Updated version
+            ),
+            instances=2,  # Number of instances in the cluster
+            default_database_name="MinecraftDB",
+            instance_props=rds.InstanceProps(  # Specify instance properties here
+                instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
+                vpc=vpc
+            ),
             removal_policy=cdk.RemovalPolicy.DESTROY,
         )
 
         # Configure security group to allow traffic from the Minecraft server to the database
-        db.connections.allow_from(service.service, ec2.Port.tcp(3306))
+        db_cluster.connections.allow_from(service.service, ec2.Port.tcp(3306))
 
         # Define a domain name for the Minecraft server
         domain_name = "allynak.infracourse.cloud"  # Replace with your desired domain name
